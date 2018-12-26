bd4aa2214590c344010f364e0bc5bbd54548c7e8
ll9 <llwierer@web.de>
2018-12-26 16:20:47 +0100

    Basic row duplication

--- DuplicateRows/Form1.Designer.cs
+++ DuplicateRows/Form1.Designer.cs
@@ -29,12 +29,53 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GridContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.DuplicateStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.GridContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(477, 254);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // GridContextMenuStrip
+            // 
+            this.GridContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DuplicateStripMenuItem});
+            this.GridContextMenuStrip.Name = "contextMenuStrip1";
+            this.GridContextMenuStrip.Size = new System.Drawing.Size(169, 26);
+            // 
+            // DuplicateStripMenuItem
+            // 
+            this.DuplicateStripMenuItem.Name = "DuplicateStripMenuItem";
+            this.DuplicateStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.DuplicateStripMenuItem.Text = "Spalte Duplizieren";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.GridContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip GridContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem DuplicateStripMenuItem;
     }
 }
 
--- DuplicateRows/Form1.cs
+++ DuplicateRows/Form1.cs
@@ -12,9 +12,62 @@ namespace DuplicateRows
 {
     public partial class Form1 : Form
     {
+        private DataTable _table;
+
         public Form1()
         {
             InitializeComponent();
+            _table = new DataTable();
+            InitTable();
+            InitGrid();
+        }
+
+        private void InitGrid()
+        {
+            dataGridView1.CellMouseClick += OpenDataGridContextMenu;
+            DuplicateStripMenuItem.Click += DuplicateStripMenuItem_Click;
+        }
+
+        private void DuplicateStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dataRow = ((DataRowView)dataGridView1.SelectedRows[0].DataBoundItem).Row;
+            var index = _table.Rows.IndexOf(dataRow);
+            for (int n = 0; n < 3; n++)
+            {
+                var row = dataRow.Table.NewRow();
+                for (int i = 0; i < row.Table.Columns.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        row[i] = dataRow[i].ToString() + n;
+                    }
+                    else
+                    {
+                        row[i] = dataRow[i];
+                    }
+                }
+                dataRow.Table.Rows.InsertAt(row, index + n + 1);
+            }
+        }
+
+        private void OpenDataGridContextMenu(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && e.ColumnIndex >= 0 && e.RowIndex >= 0)
+            {
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[e.RowIndex].Selected = true;
+                GridContextMenuStrip.Show(Cursor.Position);
+            }
+        }
+
+
+        private void InitTable()
+        {
+            _table.Columns.Add("col1");
+            _table.Columns.Add("col2");
+            _table.Columns.Add("col3");
+
+            dataGridView1.DataSource = _table;
         }
     }
 }
